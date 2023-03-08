99593f6cbe82d2f7a91218b259716ed8bb360954
Lucas-DCC-Pessego <lucasmaia@dcc.ufmg.br>
2023-03-08 15:31:24 -0300

    Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -7,12 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", 
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App", 
                    layout=[[label], 
                            [input_box, add_button],
-                           [list_box, edit_button]], 
+                           [list_box, edit_button, complete_button], 
+                           [exit_button]], 
                    font=('Helvetica', 14))
 while True:
     event, values = window.read()
@@ -23,17 +25,27 @@ while True:
         new_todo = values['todo'] + "\n"
         todos.append(new_todo)
         functions.write_todos(todos)
+        window['todo'].update(value='')
         window['todos'].update(values=todos)
     elif event == "Edit":
-        todo_to_edit = values["todos"][0][:-1]
+        todo_to_edit = values["todos"][0]
         new_todo = values["todo"]
         todos = functions.get_todos()
         index = todos.index(todo_to_edit)
         todos[index] = new_todo + "\n"
         functions.write_todos(todos)
         window['todos'].update(values=todos)
-    elif event =="todos":
-        window["todo"].update(value=values["todos"][0])
+    elif event == "todos":
+        window["todo"].update(value=values["todos"][0][:-1])
+    elif event == "Complete":
+        todo_to_complete = values["todos"][0]
+        todos = functions.get_todos()
+        todos.remove(todo_to_complete)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        window['todo'].update(value='')
+    elif event == "Exit":
+        break
     elif event == sg.WIN_CLOSED:
         break
 
